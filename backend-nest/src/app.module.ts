2f7495de14272c238fbb5f9eb22f792b6fbfdd82
dou2020 <doueduar2018@gmail.com>
2026-01-19 21:58:15 -0600

    add module of comments and posts, config main

--- backend-nest/src/app.module.ts
+++ backend-nest/src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { CommentsModule } from './comments/comments.module';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, 
+      envFilePath: '.env',
+    }),
+    CommentsModule,
+    PostsModule
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- backend-nest/src/main.ts
+++ backend-nest/src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Set global prefix for all routes
+  app.setGlobalPrefix('api');
+
+  // Esto te dará una UI para probar tus endpoints en /api/docs
+  const config = new DocumentBuilder()
+    .setTitle('API REST')
+    .setDescription('Documentación de la API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
+  // Enable CORS for all origins
+  app.enableCors();
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
